8990017410566bf2420d31c07e6e1223ba11675a
EggsyOnCode <farzansaqib2003@gmail.com>
2023-09-22 17:30:50 +0500

    fixed env file

--- app/server.js
+++ app/server.js
@@ -1,78 +1,96 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
+require('dotenv').config();
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 let app = express();
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/profile-picture', function (req, res) {
+app.get("/profile-picture", function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  res.end(img, "binary");
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = `mongodb://admin:${process.env.PASSWORD}@localhost:27017`;
 
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+let mongoUrlDocker = `mongodb://admin:${process.env.PASSWORD}!@mongodb`;
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
+let databaseName = "test-app";
 
-app.post('/update-profile', function (req, res) {
+app.post("/update-profile", function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-    userObj['userid'] = 1;
-
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
-
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+  MongoClient.connect(
+    mongoUrlLocal,
+    mongoClientOptions,
+    function (err, client) {
       if (err) throw err;
-      client.close();
-    });
 
-  });
+      let db = client.db(databaseName);
+      userObj["userid"] = 1;
+
+      let myquery = { userid: 1 };
+      let newvalues = { $set: userObj };
+
+      db.collection("users").updateOne(
+        myquery,
+        newvalues,
+        { upsert: true },
+        function (err, res) {
+          if (err) throw err;
+          client.close();
+        }
+      );
+    }
+  );
   // Send response
   res.send(userObj);
 });
 
-app.get('/get-profile', function (req, res) {
+app.get("/get-profile", function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-
-    let myquery = { userid: 1 };
-
-    db.collection("users").findOne(myquery, function (err, result) {
+  MongoClient.connect(
+    mongoUrlLocal,
+    mongoClientOptions,
+    function (err, client) {
       if (err) throw err;
-      response = result;
-      client.close();
 
-      // Send response
-      res.send(response ? response : {});
-    });
-  });
+      let db = client.db(databaseName);
+
+      let myquery = { userid: 1 };
+
+      db.collection("users").findOne(myquery, function (err, result) {
+        if (err) throw err;
+        response = result;
+        client.close();
+
+        // Send response
+        res.send(response ? response : {});
+      });
+    }
+  );
 });
 
 app.listen(3000, function () {
